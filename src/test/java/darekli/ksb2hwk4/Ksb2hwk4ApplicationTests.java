75c160a011aa031f1c17655f01630c2852cc232a
Darek Li <darlicki@gmail.com>
2019-10-07 16:52:49 +0200

    Initial commit

---
+++ src/test/java/darekli/ksb2hwk4/Ksb2hwk4ApplicationTests.java
@@ -0,0 +1,16 @@
+package darekli.ksb2hwk4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ksb2hwk4ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
